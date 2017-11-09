1195438660ded804a8e6417df8b9ce72ca5493eb
SCZ-MSI <shaoshixin842@live.com>
2017-11-09 22:37:58 +0800

    登录界面没做好

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.render('web/index');
+  res.render('web/login/login');
 });
 exports.test = function(erq,res){
   res.send('Hello World!');
