089b3222d1c09eb5dc849a304106a42717ebadbe
SCZ-MSI <shaoshixin842@live.com>
2017-12-26 23:37:45 +0800

    1、登录界面除了样式做好了，主要收获有：单个表单中如果有多个按钮（用于提交或者用于触发其他事件），可以将除提交外的其他按钮以button的方式绑定事件。注意阻止表单提交。 2、layui中layer，element，form模块 form.on('',fun(){}); 3、jQuery：$(document).on('click','# ',fun(){});

--- public/web/js/js.js
+++ public/web/js/js.js
@@ -1,15 +1,26 @@
-layui.use('element', function () {
+layui.use(['form', 'layer', 'element'], function () {
+    var $ = layui.jquery;
     var element = layui.element; //导航的hover效果、二级菜单等功能，需要依赖element模块
+    var layer = layui.layer;
+    var form = layui.form;
     //监听导航点击
-    // element.on('nav(demo)', function (elem) {
-    //     console.log(elem)
-    //     // layer.msg(elem.text());
-    // });
-});
-layui.use('form',function () {
-var form = layui.form;
-    form.on('submit(btn-login)',function (data) {
-        data.form.action="login/loginSub";
+    element.on('nav(nav)', function (elem) {
+        console.log(elem);
+        layer.msg("123");
+    });
+    form.on('submit(btn-login)', function (data) {
+        data.form.action = "login/loginSub";
         data.form.submit();
+    });
+    // $(document).on('click','#btn-login',function () {
+    //     document.login_form.action = "login/loginSub";
+    //     document.login_form.submit();
+    // });
+    $(document).on('click','#btn-register',function () {
+        //阻止表单提交，显示注册界面
+        var event = event || window.event;
+        event.preventDefault(); // 兼容标准浏览器
+        window.event.returnValue = false; // 兼容IE6~8
+       layer.msg("注册");
     });
 });
